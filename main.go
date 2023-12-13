cc7b026189ed7e3323a8478d53c2e74558b0f7f1
gersondantas <gerson.accari@hotmail.com>
2023-12-13 19:30:13 -0300

    feat: change to smaller image

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(rw, "Full Cycle Rocks!!\n")
-	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	fmt.Print("Full Cycle Rocks!!\n")
 }
